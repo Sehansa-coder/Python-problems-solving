165e6b1c87d84c78de4783cfbae92a54e0c4c93e
D.B.S.Dewlini <sehansadewlini27@gmail.com>
2025-12-21 00:19:29 +0530

    Add more functional questions

---
+++ Functions/Printing_pattern.py
@@ -0,0 +1,30 @@
+# ( BSC In Information and Technology- Higher Diploma Year 1 question)
+
+
+# Problem:
+# Write a progrsm to implement a function called drawLines() to draw 
+# the below figure. And call the drawLines() function to draw the rectangle
+
+#  ********** 
+#  ********** 
+#  ********** 
+#  ********** 
+
+# To implement the function you can use repetition structures such as for-loop structure. 
+
+def pattern():
+    for i in range(4):
+        for a in range(11):
+            print("*",end="")
+        print()
+
+# call the function
+pattern()
+
+
+# Sample output:
+#-----------------------------
+#  ***********
+#  ***********
+#  ***********
+#  ***********
