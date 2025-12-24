12140b71eb3e4574da8ce7961ca8aa011552d4a3
D.B.S.Dewlini <sehansadewlini27@gmail.com>
2025-12-24 19:44:39 +0530

    Add a program to roll a die

---
+++ Functions/Playboard_game.py
@@ -0,0 +1,22 @@
+# ------------------ By Codewars-------------
+
+# Problem:
+# In this game, the hero moves from left to right. The player rolls the die and moves the number of spaces indicated by the die two times.
+
+# Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+# Example: move(3, 6) should equal 15
+
+# It means correny position+ roll +roll
+
+
+def move_hero(current_position,roll):
+    return current_position+(roll+roll)
+
+current_position=int(input("Enter current position: "))
+roll=int(input("Enter rolled value:"))
+print(move_hero(current_position,roll))
+
+# sample output:
+# Enter current position: 3
+# Enter rolled value:6
+# 15
